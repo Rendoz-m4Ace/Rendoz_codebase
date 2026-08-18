f4ce5cc6b640e9cdefe68e83c4acf6e435677227
PeacefulCoder <afolabipeace203@gmail.com>
2026-08-18 14:43:14 +0100

    Modified page.tsx, created a new folder-assets and added new files, created 2 files under component folder-RentCategory, MarketPlace

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import TrustFeatures from "@/component/TrustFeatures";
 import HowItWorks from "@/component/HowItWorks";
 import FAQSection from "@/component/FAQSection";
 import WaitlistFooter from "@/component/WaitlistFooter";
+import RentCategory from "@/component/RentCategory";
+import MarketPlace from "@/component/MarketPlace";
  
 export default function Home() {
   return (
     <main>
+      <RentCategory />
+      <MarketPlace />
       <TrustFeatures />
       <HowItWorks />
       <FAQSection />
